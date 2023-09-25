94ae86beba7c8bfbe867e52b83402b019ac55bcf
Naved Ansari <naved001@gmail.com>
2023-09-25 16:57:40 -0400

    add ability to exclude projects for some duration

--- README.md
+++ README.md
@@ -21,9 +21,17 @@ bar
 blah blah
 ```
 
+A file containing list of timed projects will looks like this:
+```
+PI,Project,Start Date,End Date,Reason
+alice@example.com,project foo,2023-09,2024-09,Internal
+bo@example.com,project bar,2023-09,2024-09,Internal
+```
+
+Note that a date `2023-09` will be read as `2023-09-01 00:00:00`. So, if a project end date is specified as `2023-09` but the script is executed on `2023-09-02`, the project will no longer be excluded (assuming we met the start date condition).
+
 The CSV report must have the headers `Manager (PI)'` and `Project - Allocation'`.
 
 ```
-usage: process_report.py [-h] --report-file REPORT_FILE --pi-file PI_FILE --projects-file PROJECTS_FILE
-```
+usage: process_report.py [-h] --report-file REPORT_FILE --pi-file PI_FILE --projects-file PROJECTS_FILE --timed-projects-file TIMED_PROJECTS_FILE```
 
--- process_report.py
+++ process_report.py
@@ -3,6 +3,8 @@ import argparse
 import csv
 import pandas
 
+from datetime import datetime
+
 def main():
     """Remove non-billable PIs and projects"""
 
@@ -22,23 +24,48 @@ def main():
         required=True,
         help="File containing list of projects that are non-billable",
     )
+    parser.add_argument(
+        "--timed-projects-file",
+        required=True,
+        help="File containing list of projects that are non-billable within a specified duration",
+    )
     args = parser.parse_args()
-    remove_non_billables(args.report_file, args.pi_file, args.projects_file)
 
-
-def remove_non_billables(report_file, pi_file, projects_file):
     pi = []
     projects = []
-
-    with open(pi_file) as file:
+    with open(args.pi_file) as file:
         pi = [line.rstrip() for line in file]
-
-    with open(projects_file) as file:
+    with open(args.projects_file) as file:
         projects = [line.rstrip() for line in file]
 
+    timed_projects_list = timed_projects(args.timed_projects_file)
+
+    projects = list(set(projects + timed_projects_list))
+
+    remove_non_billables(args.report_file, pi, projects)
+
+
+def timed_projects(timed_projects_file):
+    """Returns list of projects that should be excluded based on dates"""
+    timed_projects_list = []
+    dataframe = pandas.read_csv(timed_projects_file)
+
+    # convert to datetime objects
+    dataframe['Start Date'] = pandas.to_datetime(dataframe['Start Date'], format="%Y-%m")
+    dataframe['End Date'] = pandas.to_datetime(dataframe['End Date'], format="%Y-%m")
+
+    current_date = datetime.now()
+    for index, row in dataframe.iterrows():
+        if row['Start Date'] <= current_date <= row['End Date']:
+            timed_projects_list.append(row['Project'])
+
+    return timed_projects_list
+
+
+def remove_non_billables(report_file, pi, projects):
     dataframe = pandas.read_csv(report_file)
     filtered_dataframe = dataframe[~dataframe['Manager (PI)'].isin(pi) & ~dataframe['Project - Allocation'].isin(projects)]
-    filtered_dataframe.to_csv(report_file + '_filtered', index=False)
+    filtered_dataframe.to_csv('filtered_' + report_file, index=False)
 
 if __name__ == "__main__":
     main()
