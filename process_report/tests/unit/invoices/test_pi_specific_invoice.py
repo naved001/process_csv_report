1f96e4b888ad68d260c81539c2e1957e1d89d711
Quan Pham <qmpham2019@gmail.com>
2024-12-19 15:47:02 -0500

    Exported PI invoices as PDFs
    The PI-specific dataframes will first be converted to HTML
    tables using Jinja templates, and then converted to PDFs using
    Chromium. Now, users of the script must provide a path to the
    Chromium/Chrome binary throught the env var `CHROME_BIN_PATH`
    
    A html template folder has been added, and the test cases
    for the PI-specific invoice will now both check whether the
    dataframe is formatted correctly and if the PDFs are
    correctly generated. The dockerfile has been to install chromium

--- process_report/tests/unit/invoices/test_pi_specific_invoice.py
+++ process_report/tests/unit/invoices/test_pi_specific_invoice.py
@@ -1,68 +1,145 @@
 from unittest import TestCase, mock
-import tempfile
 import pandas
-import os
 
 from process_report.tests import util as test_utils
 
 
-class TestExportPICSV(TestCase):
-    def setUp(self):
-        data = {
-            "Invoice Month": ["2023-01", "2023-01", "2023-01", "2023-01", "2023-01"],
-            "Manager (PI)": ["PI1", "PI1", "PI1", "PI2", "PI2"],
-            "Institution": ["BU", "BU", "BU", "HU", "HU"],
-            "Project - Allocation": [
-                "ProjectA",
-                "ProjectB",
-                "ProjectC",
-                "ProjectD",
-                "ProjectE",
+class TestPISpecificInvoice(TestCase):
+    def _get_test_invoice(
+        self,
+        pi,
+        institution,
+        balance,
+        is_billable=None,
+        missing_pi=None,
+        group_name=None,
+    ):
+        if not is_billable:
+            is_billable = [True] * len(pi)
+
+        if not missing_pi:
+            missing_pi = [False] * len(pi)
+
+        if not group_name:
+            group_name = [None] * len(pi)
+
+        return pandas.DataFrame(
+            {
+                "Manager (PI)": pi,
+                "Institution": institution,
+                "Is Billable": is_billable,
+                "Missing PI": missing_pi,
+                "Prepaid Group Name": group_name,
+                "Prepaid Group Institution": ["" for _ in range(len(pi))],
+                "Prepaid Group Balance": [0 for _ in range(len(pi))],
+                "Prepaid Group Used": [0 for _ in range(len(pi))],
+                "Balance": balance,
+            }
+        )
+
+    def test_get_pi_dataframe(self):
+        def add_dollar_sign(data):
+            if pandas.isna(data):
+                return data
+            else:
+                return "$" + str(data)
+
+        test_invoice = self._get_test_invoice(
+            ["PI1", "PI1", "PI2", "PI2"],
+            [
+                "BU",
+                "BU",
+                "HU",
+                "HU",
             ],
-            "Untouch Data Column": ["DataA", "DataB", "DataC", "DataD", "DataE"],
-            "Is Billable": [True, True, True, True, True],
-            "Missing PI": [False, False, False, False, False],
-        }
-        self.dataframe = pandas.DataFrame(data)
-        self.invoice_month = data["Invoice Month"][0]
+            [100, 200, 300, 400],
+            group_name=[None, "G1", None, None],
+        )
+        answer_invoice_pi1 = (
+            test_invoice[test_invoice["Manager (PI)"] == "PI1"]
+            .copy()
+            .reset_index(drop=True)
+        )
+        answer_invoice_pi1.loc[len(answer_invoice_pi1)] = None
+        answer_invoice_pi1.loc[
+            answer_invoice_pi1.index[-1], ["Invoice Month", "Balance"]
+        ] = ["Total", 300]
+        for column_name in [
+            "Prepaid Group Balance",
+            "Prepaid Group Used",
+            "Balance",
+        ]:
+            answer_invoice_pi1[column_name] = answer_invoice_pi1[column_name].apply(
+                add_dollar_sign
+            )
+        answer_invoice_pi1.fillna("", inplace=True)
+
+        answer_invoice_pi2 = (
+            test_invoice[test_invoice["Manager (PI)"] == "PI2"]
+            .copy()
+            .reset_index(drop=True)
+        )
+        answer_invoice_pi2.loc[len(answer_invoice_pi2)] = None
+        answer_invoice_pi2.loc[
+            answer_invoice_pi2.index[-1], ["Invoice Month", "Balance"]
+        ] = ["Total", 700]
+        answer_invoice_pi2 = answer_invoice_pi2.drop(
+            [
+                "Prepaid Group Name",
+                "Prepaid Group Institution",
+                "Prepaid Group Balance",
+                "Prepaid Group Used",
+            ],
+            axis=1,
+        )
+        answer_invoice_pi2["Balance"] = answer_invoice_pi2["Balance"].apply(
+            add_dollar_sign
+        )
+        answer_invoice_pi2.fillna("", inplace=True)
+
+        pi_inv = test_utils.new_pi_specific_invoice(data=test_invoice)
+        output_invoice = pi_inv._get_pi_dataframe(test_invoice, "PI1")
+        self.assertTrue(answer_invoice_pi1.equals(output_invoice))
+
+        output_invoice = pi_inv._get_pi_dataframe(test_invoice, "PI2")
+        self.assertTrue(answer_invoice_pi2.equals(output_invoice))
 
     @mock.patch("process_report.invoices.invoice.Invoice._filter_columns")
-    def test_export_pi(self, mock_filter_cols):
-        mock_filter_cols.return_value = self.dataframe
+    @mock.patch("os.path.exists")
+    @mock.patch("subprocess.run")
+    def test_export_pi(self, mock_subprocess_run, mock_path_exists, mock_filter_cols):
+        invoice_month = "2024-10"
+        test_invoice = self._get_test_invoice(
+            ["PI1", "PI1", "PI2", "PI2"],
+            [
+                "BU",
+                "BU",
+                "HU",
+                "HU",
+            ],
+            [100, 200, 300, 400],
+            group_name=[None, "G1", None, None],
+        )
 
-        output_dir = tempfile.TemporaryDirectory()
+        mock_filter_cols.return_value = test_invoice
+        mock_path_exists.return_value = True
+        test_dir = "test_dir"
+
         pi_inv = test_utils.new_pi_specific_invoice(
-            output_dir.name, invoice_month=self.invoice_month, data=self.dataframe
+            test_dir, invoice_month, data=test_invoice
         )
         pi_inv.process()
         pi_inv.export()
-        pi_csv_1 = f'{self.dataframe["Institution"][0]}_{self.dataframe["Manager (PI)"][0]} {self.dataframe["Invoice Month"][0]}.csv'
-        pi_csv_2 = f'{self.dataframe["Institution"][3]}_{self.dataframe["Manager (PI)"][3]} {self.dataframe["Invoice Month"][3]}.csv'
-        self.assertIn(pi_csv_1, os.listdir(output_dir.name))
-        self.assertIn(pi_csv_2, os.listdir(output_dir.name))
-        self.assertEqual(
-            len(os.listdir(output_dir.name)),
-            len(self.dataframe["Manager (PI)"].unique()),
-        )
+        pi_pdf_1 = f"{test_dir}/BU_PI1_{invoice_month}.pdf"
+        pi_pdf_2 = f"{test_dir}/HU_PI2_{invoice_month}.pdf"
 
-        pi_df = pandas.read_csv(output_dir.name + "/" + pi_csv_1)
-        self.assertEqual(len(pi_df["Manager (PI)"].unique()), 1)
-        self.assertEqual(
-            pi_df["Manager (PI)"].unique()[0], self.dataframe["Manager (PI)"][0]
-        )
-
-        self.assertIn("ProjectA", pi_df["Project - Allocation"].tolist())
-        self.assertIn("ProjectB", pi_df["Project - Allocation"].tolist())
-        self.assertIn("ProjectC", pi_df["Project - Allocation"].tolist())
-
-        pi_df = pandas.read_csv(output_dir.name + "/" + pi_csv_2)
-        self.assertEqual(len(pi_df["Manager (PI)"].unique()), 1)
-        self.assertEqual(
-            pi_df["Manager (PI)"].unique()[0], self.dataframe["Manager (PI)"][3]
-        )
-
-        self.assertIn("ProjectD", pi_df["Project - Allocation"].tolist())
-        self.assertIn("ProjectE", pi_df["Project - Allocation"].tolist())
-        self.assertNotIn("ProjectA", pi_df["Project - Allocation"].tolist())
-        self.assertNotIn("ProjectB", pi_df["Project - Allocation"].tolist())
-        self.assertNotIn("ProjectC", pi_df["Project - Allocation"].tolist())
+        for i, pi_pdf_path in enumerate([pi_pdf_1, pi_pdf_2]):
+            chrome_arglist, _ = mock_subprocess_run.call_args_list[i]
+            answer_arglist = [
+                "/usr/bin/chromium",
+                "--headless",
+                "--no-sandbox",
+                f"--print-to-pdf={pi_pdf_path}",
+                "--no-pdf-header-footer",
+            ]
+            self.assertTrue(answer_arglist == chrome_arglist[0][:-1])
